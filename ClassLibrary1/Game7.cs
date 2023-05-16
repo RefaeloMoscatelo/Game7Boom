3d31cfb95da9ffe0e99caedba1717baf687bdc5c
Rafael <refaelomoscatelo@gmail.com>
2023-05-16 20:50:08 +0300

    Add project files.

---
+++ ClassLibrary1/Game7.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ClassLibrary1
+{
+    public delegate void ReportGameHandler(int x);
+   public class Game7
+    {
+       public event ReportGameHandler ReportGame;
+
+       public void Game()
+       {
+           for (int i = 1; i <= 200; i++)
+           {             
+             
+               Thread.Sleep(500);
+               ReportGame(i);
+           }
+       }
+    }
+}
---
+++ Game7Boom/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace Game7Boom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game7 r = new Game7();
+            r.ReportGame += Print;
+            r.Game();
+        }
+        static void Print(int ret)
+        {
+            string str = "";
+            if (ret%7==0|| ret.ToString().Contains("7"))
+            {
+                 str = "Boom";
+                Console.Write(str);
+               
+            }
+                
+            else
+            {
+                
+                Console.Write("{0}",ret);
+            } 
+            Console.Write("   \r");
+        }
+    }
+}
